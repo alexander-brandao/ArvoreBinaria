5cdda137278e0cc305fee2efb9c69f9d4cbf162d
Alex Brandão <alex12a7x@gmail.com>
2021-03-03 19:57:41 -0300

    iniciando arvore

--- ArvoreBinaria.cpp
+++ ArvoreBinaria.cpp
@@ -27,10 +27,82 @@ struct node {
 //craindo apelido para o nó da arvore
 typedef struct node Node;
 
+// funcao para inicializar a arvore
+Node *initialize() {
+
+    return NULL;
+}
+
+//funcao que retorna "item" (conteudo do Node)
+Item itemCreate(int cod){
+    Item item;
+    item.cod = cod;
+    return item;
+}
+
+//funca para inserir elementos na arvore
+Node *insert(Node *root, Item x) {
+
+    //caso a arvore estaja vazia
+    if (root == NULL) {
+        
+        //alocação dinamica na memoria para a struct "Node"
+        Node* aux = (Node*)malloc(sizeof(Node)); 
+        aux->item = x;
+        aux->left = NULL;
+        aux->right = NULL;
+        return aux;
+    }
+    else {
+        if (x.cod > root->item.cod) {
+           root->right = insert(root->right, x);
+        }
+        else if (x.cod < root->item.cod) {
+            root->left = insert(root->left, x);
+        }
+        else {
+            printf("Node com valor repetido");
+        }
+    }
+
+    return root;
+}
+
+//funcao que mostra a arvore 
+void treePrint(Node* root) {
+
+    if (root != NULL) {
+        printf("%d ", root->item);
+        treePrint(root->left);
+        treePrint(root->right);
+    }
+}
+
+//funcao para liberar a arvore
+void treeFree(Node *root){
+    
+    if (root != NULL) {
+        treeFree(root->left);
+        treeFree(root->right);
+       // funcao free executando por ultimo para não perder as referencias "left and right" 
+        free(root);
+    }
+}
+
+
+
 int main(){
 
-    Node* root = NULL;
+    //inciando a arvore
+    Node* root = initialize();
+    root = insert(root, itemCreate(10));
+    root = insert(root, itemCreate(9));
+    root = insert(root, itemCreate(20));
+    root = insert(root, itemCreate(30));
+    
 
+    treePrint(root);
+    treeFree(root);
     return 0;
 }
 
